1028988770c98c6d4e8518bbf571398dd25c19fe
Ali Merkouk <63125615+Sava-Jcript@users.noreply.github.com>
2023-05-31 23:37:34 +0200

    minor changes

--- pages/tickets/bookingStep3.js
+++ pages/tickets/bookingStep3.js
@@ -1,12 +1,13 @@
 import React from "react";
-import sendDataDB from "../../components/api/SupaBase"
+import sendDataDB from "../../components/api/SupaBase";
 import { useRouter } from "next/router";
 import CulculateFunction from "../../components/UI-cards/calculatefunction";
 import NavBar from "../../components/nav-bar/NavBar";
 
-function bookingStep4(props) {
-    const router = useRouter();
-    async function confirmBooking() {
+function BookingStep4(props) {
+  const router = useRouter();
+
+  async function confirmBooking() {
     const request = await fetch("https://bittersweet-painted-willow.glitch.me/fullfill-reservation", {
       method: "POST",
       headers: {
@@ -14,6 +15,7 @@ function bookingStep4(props) {
       },
       body: JSON.stringify({ id: props.orderInfo.orderID }),
     });
+
     const response = await request.json();
     const message = response.message;
 
@@ -29,86 +31,76 @@ function bookingStep4(props) {
 
     if (message === "Reservation completed") {
       router.push("/tickets/bookingStep4");
-      const response = await sendDataDB(payload);
-
+      await sendDataDB(payload);
     }
+
     if (message === "ID not found") {
       router.push("/tickets/bookingStep4");
     }
   }
 
-
-  function CancelMethod() {
+  function cancelMethod() {
     router.push("/tickets/bookingStep2");
   }
 
-
   return (
     <div>
+      <NavBar />
+      <h2>Payment Information</h2>
+      <h5>Please Enter Your Credit Card Information.</h5>
+      <div>
+        <h3>Credit Card Details</h3>
+        <form>
+          <div>
+            <label>
+              Name On Card
+              <input required type="text" name="name" id="form-name" placeholder="Eren Jæger" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Card Number
+              <input required mask="9999 9999 9999 9999" maskChar={null} placeholder={"1234 1234 1234 1234"} name="cardNo" id="form-cardNo" onBlur={props.verify} />
+            </label>
 
-<NavBar/>
-        <h2>Payment Information</h2>
-        <h5>Please Enter Your Credit Card Information.</h5>
-        <div >
-          <h3>Credit Card Details</h3>
-          <form>
-            <div>
-              <label >
-                Name On Card
-                <input required type="text" name="name" id="form-name" placeholder="Eren Jæger" />
-              </label>
-            </div>
-            <div>
-              <label >
-                Card Number
-                <input required mask="9999 9999 9999 9999" maskChar={null} placeholder={"1234 1234 1234 1234"} name="cardNo" id="form-cardNo" onBlur={props.verify} />
-              </label>
+            <label>
+              Expiry Date
+              <input required mask="99/99" maskChar={null} placeholder={"12/34"} name="expiry" id="form-expiry" onBlur={props.verify} />
+            </label>
 
-              <label>
-                Expiry Date
-                <input required mask="99/99" maskChar={null} placeholder={"12/34"} name="expiry" id="form-expiry" onBlur={props.verify} />
-              </label>
+            <label>
+              CVC Number
+              <input required type="text" name="cvc" id="form-cvc" inputMode="numeric" maxLength="3" onBlur={props.verify} placeholder={123} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Email
+              <input required type="email" name="email" id="form-email" onBlur={props.verify} placeholder={"Eren-Jæger@yahoo.dk"} />
+            </label>
 
-              <label >
-                CVC Number
-                <input required type="text" name="cvc" id="form-cvc" inputMode="numeric" maxLength="3" onBlur={props.verify} placeholder={123} />
-              </label>
-            </div>
-            <div>
-              <label >
-                Email
-                <input required type="email" name="email" id="form-email" onBlur={props.verify} placeholder={"Eren-Jæger@yahoo.dk"} />
-              </label>
+            <label>
+              Phone Number
+              <input mask="99 99 99 99" maskChar={null} required type="text" name="phone" id="form-phone" placeholder={"01 23 45 67"} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Billing Address
+              <textarea required name="address" id="form-address" placeholder="Guldbergsgade 29N, 2200 København" />
+            </label>
+          </div>
+        </form>
+      </div>
 
-              <label >
-                Phone Number
-                <input mask="99 99 99 99" maskChar={null} required type="text" name="phone" id="form-phone" placeholder={"01 23 45 67"} />
-              </label>
-            </div>
-            <div>
-              <label>
-                Billing Address
-                <textarea required name="address" id="form-address" placeholder="Guldbergsgade 29N, 2200 København" />
-              </label>
-            </div>
-          </form>
-        </div>
-
-      {
-        <CulculateFunction orderInfo={props.orderInfo} setOrderInfo={props.setOrderInfo} tentPrice={props.tentPrice} setUpPrice={props.setUpPrice} />
-      }
-    
+      <CulculateFunction orderInfo={props.orderInfo} setOrderInfo={props.setOrderInfo} tentPrice={props.tentPrice} setUpPrice={props.setUpPrice} />
 
       <div>
-        <button  onClick={CancelMethod}>
-          Back
-        </button>
-        <button  onClick={confirmBooking}>
-          Confirm Payment 
-        </button>
+        <button onClick={cancelMethod}>Back</button>
+        <button onClick={confirmBooking}>Confirm Payment</button>
       </div>
     </div>
   );
 }
 
-export default bookingStep4;
+export default BookingStep4;
