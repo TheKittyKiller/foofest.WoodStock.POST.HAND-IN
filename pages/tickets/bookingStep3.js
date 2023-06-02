ad65e4f4570742e6ec4d07fbbae80052d61ccdc9
Yani <yanivlascogmail.com>
2023-06-02 04:14:43 +0200

    last page almost done

--- pages/tickets/bookingStep3.js
+++ pages/tickets/bookingStep3.js
@@ -48,62 +48,71 @@ function BookingStep4(props) {
 
   return (
     <div>
+      <div className={styles.main}>
       <NavBar />
-      <h2 className={styles.zabi}>Payment Information</h2>
-      <h5>Please Enter Your Credit Card Information.</h5>
-      <div>
-        <h3>Credit Card Details</h3>
-        <form>
+      <div className={styles.first}>
+          <h2 className={styles.zabi}>Payment Information</h2>
+          <h5>Please Enter Your Credit Card Information.</h5>
+          <h3>Credit Card Details</h3>
           <div>
-            <label htmlFor="form-name">
-              Name On Card
-              <input required type="text" name="name" id="form-name" placeholder="Eren Jæger" />
-            </label>
-          </div>
-          <div>
-            <label>
-              Card Number
-              <InputMask required mask="9999 9999 9999 9999" maskChar={null} placeholder={"1234 1234 1234 1234"} name="cardNo" id="form-cardNo" onBlur={props.verify} />
-            </label>
-          </div>
-          <div>
-            <label>
-              Expiry Date
-              <InputMask required mask="99/99" maskChar={null} placeholder={"12/34"} name="expiry" id="form-expiry" onBlur={props.verify} />
-            </label>
-          </div>
-          <div>
-            <label>
-              CVC Number
-              <input required type="text" name="cvc" id="form-cvc" inputMode="numeric" maxLength="3" onBlur={props.verify} placeholder={123} />
-            </label>
-          </div>
-          <div>
-            <label>
-              Email
-              <input required type="email" name="email" id="form-email" onBlur={props.verify} placeholder={"Eren-Jæger@yahoo.dk"} />
-            </label>
-          </div>
-          <div>
-            <label>
-              Phone Number
-              <InputMask mask="99 99 99 99" maskChar={null} required type="text" name="phone" id="form-phone" placeholder={"01 23 45 67"} />
-            </label>
-          </div>
-          <div>
-            <label>
-              Billing Address
-              <textarea required name="address" id="form-address" placeholder="Guldbergsgade 29N, 2200 København" />
-            </label>
-          </div>
-        </form>
       </div>
+        
+          <div className={styles.card}>
+            
+            <form>
+              <div>
+                <label htmlFor="form-name">
+                  Name On Card
+                  <input required type="text" name="name" id="form-name" placeholder="Eren Jæger" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Card Number
+                  <InputMask required mask="9999 9999 9999 9999" maskChar={null} placeholder={"1234 1234 1234 1234"} name="cardNo" id="form-cardNo" onBlur={props.verify} />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Expiry Date
+                  <InputMask required mask="99/99" maskChar={null} placeholder={"12/34"} name="expiry" id="form-expiry" onBlur={props.verify} />
+                </label>
+              </div>
+              <div>
+                <label>
+                  CVC Number
+                  <input required type="text" name="cvc" id="form-cvc" inputMode="numeric" maxLength="3" onBlur={props.verify} placeholder={123} />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Email
+                  <input required type="email" name="email" id="form-email" onBlur={props.verify} placeholder={"Eren-Jæger@yahoo.dk"} />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Phone Number
+                  <InputMask mask="99 99 99 99" maskChar={null} required type="text" name="phone" id="form-phone" placeholder={"01 23 45 67"} />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Billing Address
+                  <textarea required name="address" id="form-address" placeholder="Guldbergsgade 29N, 2200 København" />
+                </label>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className={styles.second}>
+          <CulculateFunction orderInfo={props.orderInfo} setOrderInfo={props.setOrderInfo} tentPrice={props.tentPrice} setUpPrice={props.setUpPrice} />
 
-      <CulculateFunction orderInfo={props.orderInfo} setOrderInfo={props.setOrderInfo} tentPrice={props.tentPrice} setUpPrice={props.setUpPrice} />
-
-      <div>
-        <button onClick={cancelMethod}>Back</button>
-        <button onClick={confirmBooking}>Confirm Payment</button>
+          <div>
+            <button onClick={cancelMethod}>Back</button>
+            <button onClick={confirmBooking}>Confirm Payment</button>
+          </div>
+        </div>
       </div>
     </div>
   );
